baa77567c67197ae635aa489291c47c7ac4d2cc7
hamza khan <hamza.khan@aircod.com>
2024-07-11 15:59:54 +0500

    Add project files.

---
+++ Signal_RR/Controllers/ServerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Signal_RR.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRApp.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public ServerController(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(string user, string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+            return RedirectToAction("Index");
+        }
+    }
+}
